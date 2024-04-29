b2662750f6698160304478bbe9523628c887a2d7
hyrki09 <hyrki0901@naver.com>
2024-04-29 20:21:36 +0900

    sbb 설정 대응

---
+++ src/main/java/sbb/springboard/HelloLombok.java
@@ -0,0 +1,21 @@
+package sbb.springboard;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloLombok {
+    String hello;
+    int lombok;
+
+
+    public static void main(String[] args){
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setHello("hellog");
+
+        System.out.println("helloLombok = " +helloLombok.getHello());
+
+    }
+}
