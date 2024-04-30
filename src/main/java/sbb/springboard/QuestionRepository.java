6b04a86ddbae894141cb8cf2ab8d9aeecffcc813
hyrki09 <hyrki0901@naver.com>
2024-04-30 18:59:48 +0900

    sbb - Entity 생성 및 QuestionRepository  테스트

--- src/main/java/sbb/springboard/QuestionRepository.java
+++ src/main/java/sbb/springboard/QuestionRepository.java
@@ -2,8 +2,14 @@ package sbb.springboard;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 // JpaRepository<Question, Integer>는 Question 엔티티로 리포지터리를 생성한다는 의미
 // 기본키가 Integer임을 이와 같이 추가로 지정
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
+    Question findBySubject(String subject);
+    Question findBySubjectAndContent(String subject, String content);
+    List<Question> findBySubjectLike(String subject);
 
 }
--- src/test/java/sbb/springboard/SpringboardApplicationTests.java
+++ src/test/java/sbb/springboard/SpringboardApplicationTests.java
@@ -1,10 +1,14 @@
 package sbb.springboard;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class SpringboardApplicationTests {
@@ -16,18 +20,85 @@ class SpringboardApplicationTests {
 	@Test
 	void contextLoads() {
 		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고싶습니다.");
+		q1.setSubject("오늘은 몇일 일까요?");
+		q1.setContent("바로 오늘은 근로자의 날 전날입니다!");
 		q1.setCreateDate(LocalDateTime.now());
 		this.questionRepository.save(q1);
 
 
-		Question q2 = new Question();
-		q2.setSubject("스프링 부트 모델 질문입니다.");
-		q2.setContent("어떻게 하면 잘해질까요?");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);
+//		Question q2 = new Question();
+//		q2.setSubject("스프링 부트 모델 질문입니다.");
+//		q2.setContent("어떻게 하면 잘해질까요?");
+//		q2.setCreateDate(LocalDateTime.now());
+//		this.questionRepository.save(q2);
 
 	}
+
+	@Test
+	void testFindAll(){
+		List<Question> all = this.questionRepository.findAll();
+		Assertions.assertEquals(2, all.size());
+
+		Question question = all.get(0);
+		Assertions.assertEquals("sbb가 무엇인가요?", question.getSubject());
+	}
+
+
+	@Test
+	void testFindById(){
+		Optional<Question> oq = this.questionRepository.findById(1);
+		if (oq.isPresent()) {
+			Question question = oq.get();
+//			Assertions.assertEquals("sbb가 무엇인가요?", question.getSubject());
+			Assertions.assertEquals("수정된 제목", question.getSubject());
+		}
+	}
+
+	@Test
+	void testFindBySubject(){
+		Question bySubject = this.questionRepository.findBySubject("오늘은 몇일 일까요?");
+
+		Assertions.assertEquals(5, bySubject.getId());
+	}
+
+	@Test
+	void testFindBySubjectAndContent(){
+		Question bySubjectAndContent = this.questionRepository.findBySubjectAndContent("오늘은 몇일 일까요?", "바로 오늘은 근로자의 날 전날입니다!");
+
+
+		Assertions.assertEquals(5, bySubjectAndContent.getId());
+	}
+
+	@Test
+	void testFindBySubjectLike(){
+		List<Question> bySubjectLike = this.questionRepository.findBySubjectLike("sbb%");
+
+		Question question = bySubjectLike.get(0);
+
+
+		Assertions.assertEquals("sbb가 무엇인가요?", question.getSubject());
+	}
+
+	@Test
+	void testModify(){
+		Optional<Question> oq = this.questionRepository.findById(1);
+		Assertions.assertTrue(oq.isPresent());
+		Question question = oq.get();
+		question.setSubject("수정된 제목");
+		this.questionRepository.save(question);
+	}
+
+	@Test
+	void testDelete(){
+
+		Assertions.assertEquals(5, this.questionRepository.count());
+		Optional<Question> oq = this.questionRepository.findById(1);
+		Assertions.assertTrue(oq.isPresent());
+		Question question = oq.get();
+		this.questionRepository.delete(question);
+		Assertions.assertEquals(4, this.questionRepository.count());
+
+	}
+
 
 }
