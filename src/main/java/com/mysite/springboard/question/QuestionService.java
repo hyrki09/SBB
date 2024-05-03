4c1b5c6367d8b4c1a987352dfdb99b246e492929
hyrki09 <hyrki0901@naver.com>
2024-05-03 15:01:13 +0900

    sbb - 상세페이지 및 예외처리

--- src/main/java/com/mysite/springboard/question/QuestionService.java
+++ src/main/java/com/mysite/springboard/question/QuestionService.java
@@ -1,10 +1,13 @@
 package com.mysite.springboard.question;
 
 
+import com.mysite.springboard.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.config.ConfigDataLocationNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,5 +18,14 @@ public class QuestionService {
 
     public List<Question> getList(){
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id){
+        Optional<Question> question = this.questionRepository.findById(id);
+        if(question.isPresent()){
+            return question.get();
+        }else{
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
