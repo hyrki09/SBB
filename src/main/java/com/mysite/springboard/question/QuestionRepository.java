00a76b0a5d6096a238a3fc73f8fce4b9ceb7721c
hyrki09 <hyrki0901@naver.com>
2024-05-09 13:49:44 +0900

    sbb - 검색기능 완료

--- src/main/java/com/mysite/springboard/question/QuestionRepository.java
+++ src/main/java/com/mysite/springboard/question/QuestionRepository.java
@@ -2,7 +2,10 @@ package com.mysite.springboard.question;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,4 +17,27 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubjectAndContent(String subject, String content);
     List<Question> findBySubjectLike(String subject);
     Page<Question> findAll(Pageable pageable);
+    Page<Question> findAll(Specification<Question> spec, Pageable pageable);
+
+
+    // Query는 반드시 테이블 기준이 아닌 엔티티 기준으로 작성해야 한다.
+    // 즉, site_user와 같은 테이블명 대신 SiteUser처럼 엔티티명을 사용해야 하고,
+    // 조인문에서 보듯이 q.author_id=u1.id와 같은 컬럼명 대신 q.author=u1처럼 엔티티의 속성명을 사용해야 한다.
+
+    //  @Query에 매개변수로 전달할 kw 문자열은 메서드의 매개변수에 @Param("kw")처럼 @Param 애너테이션을 사용해야 한다.
+    //  검색어를 의미하는 kw 문자열은 @Query 안에서 :kw로 참조된다.
+
+    @Query("select "
+            + "distinct q "
+            + "from Question q "
+            + "left outer join SiteUser u1 on q.author=u1 "
+            + "left outer join Answer a on a.question=q "
+            + "left outer join SiteUser u2 on a.author=u2 "
+            + "where "
+            + "   q.subject like %:kw% "
+            + "   or q.content like %:kw% "
+            + "   or u1.username like %:kw% "
+            + "   or a.content like %:kw% "
+            + "   or u2.username like %:kw% ")
+    Page<Question> findAllByKeyword(@Param("kw") String kw, Pageable pageable);
 }
--- src/main/java/com/mysite/springboard/question/QuestionService.java
+++ src/main/java/com/mysite/springboard/question/QuestionService.java
@@ -2,13 +2,16 @@ package com.mysite.springboard.question;
 
 
 import com.mysite.springboard.DataNotFoundException;
+import com.mysite.springboard.answer.Answer;
 import com.mysite.springboard.user.SiteUser;
+import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.config.ConfigDataLocationNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -23,11 +26,12 @@ public class QuestionService {
     private final QuestionRepository questionRepository;
 
 
-    public Page<Question> getList(int page){
+    public Page<Question> getList(int page,String kw){
         ArrayList<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("createDate"));
         Pageable pageable = PageRequest.of(page,10,Sort.by(sorts));
-        return this.questionRepository.findAll(pageable);
+        Specification<Question> spec = search(kw);
+        return this.questionRepository.findAll(spec, pageable);
     }
 
     public Question getQuestion(Integer id){
@@ -62,5 +66,25 @@ public class QuestionService {
     public void vote(Question question, SiteUser siteUser) {
         question.getVoter().add(siteUser);
         this.questionRepository.save(question);
+    }
+
+    private Specification<Question> search(String kw) {
+        return new Specification<>() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public Predicate toPredicate(Root<Question> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                query.distinct(true); // 중복제거
+                Join<Question, SiteUser> u1 = root.join("author", JoinType.LEFT);
+                Join<Question, Answer> a = root.join("answerList", JoinType.LEFT);
+                Join<Answer, SiteUser> u2 = a.join("author", JoinType.LEFT);
+                return criteriaBuilder.or(criteriaBuilder.like(root.get("subject"),"%" + kw + "%"), // 제목
+                        criteriaBuilder.like(root.get("content"),"%" + kw + "%"),  // 내용
+                        criteriaBuilder.like(u1.get("username"),"%" + kw + "%"),  // 질문 작성자
+                        criteriaBuilder.like(a.get("content"),"%" + kw + "%"),    // 답변 내용
+                        criteriaBuilder.like(u2.get("username"),"%" + kw + "%")); // 답변 작성자
+            }
+        };
+
     }
 }
